685dae6222b5023b3c260961453e2526de153be1
Systemerrorbd <96164373+Systemerrorbd@users.noreply.github.com>
2024-12-23 07:56:00 -0300

    Update strings.py

--- TechVJ/strings.py
+++ TechVJ/strings.py
@@ -2,34 +2,26 @@
 # Subscribe YouTube Channel For Amazing Bot https://t.me/SystemErrorBD
 # Ask Doubt on telegram @Mr_ERR0RR
 
-HELP_TXT = """**🌟 Help Menu** 
-
-**__FOR PRIVATE CHATS__**
-
-__first send invite link of the chat (unnecessary if the account of string session already member of the chat)
-then send post/s link__
-
-
-**__FOR BOT CHATS__**
-
-__send link with '/b/', bot's username and message id, you might want to install some unofficial client to get the id like below__
-
-```
-https://t.me/b/botusername/4321
-```
-
-**__MULTI POSTS__**
-
-__send public/private posts link as explained above with formate "from - to" to send multiple messages like below__
-
-
-```
-https://t.me/xxxx/1001-1010
-
-https://t.me/c/xxxx/101 - 120
-```
-
-__note that space in between doesn't matter__"""
+HELP_TXT = """┌————  𝐇𝐞𝐥𝐩 𝐌𝐞𝐧𝐮 🔍
+│
+├ 𝐏𝐑𝐈𝐕𝐀𝐓𝐄 𝐂𝐇𝐀𝐓𝐒:  
+├ First send invite link of the chat (unnecessary if the account of string session already member of the chat)
+├ then send post link  
+│
+├ 𝐅𝐎𝐑 𝐁𝐎𝐓 𝐂𝐇𝐀𝐓𝐒:  
+├ Send link with '/b/', bot's username and message id, you might want to install some unofficial client to get the id like below  
+│    
+├ https://t.me/b/botusername/4321  
+│
+├ 𝐏𝐎𝐒𝐓𝐒:  
+├ Public or private posts link as explained above with format "from - to" to send multiple messages like below
+│    
+├ https://t.me/xxxx/1001-1010  
+├ https://t.me/c/xxxx/101-120  
+│
+├ That space in between doesn't matte!
+│
+└—————————✘"""
        
 # Don't Remove Credit Tg - @SystemErrorBD
 # Subscribe YouTube Channel For Amazing Bot https://t.me/SystemErrorBD
